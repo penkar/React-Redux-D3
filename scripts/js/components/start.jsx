9dbd0fab8181f70f1233ea7cd5cf808b21ed4a0d
jsp <jspenkar@gmail.com>
2016-06-26 17:55:39 -0500

    Initial components added.

---
+++ scripts/js/components/start.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import D3 from 'd3';
+
+class Start extends React.Component {
+  render() {
+    return <h1>Start</h1>
+  }
+}
+
+export default Start;
---
+++ scripts/js/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Start from './components/start.jsx';
+
+const target = document.getElementById('app');
+
+render(
+  <Start />,
+  target
+);
